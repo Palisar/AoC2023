83cf937f5341ca6f054dd05c28cafe3ed8c1b51b
paul.keating <paul.keating@myhome.ie>
2023-12-06 14:27:04 +0000

    Day 6 completed

--- AdventOfCode/Day06.cs
+++ AdventOfCode/Day06.cs
@@ -19,29 +19,96 @@ Distance:  9  40  200
 
     public ValueTask<string> Part1(string[] input)
     {
-        HoldTheButton(input);
-        return ValueTask.FromResult("");
+        var winning = HoldTheButton(input);
+        return ValueTask.FromResult($"{winning}");
     }
 
     public string HoldTheButton(string[] input)
     {
         var times = input[0].Split(":")[1].Trim().Split(" ").Where(x => !string.IsNullOrEmpty(x)).ToArray();
         var distances = input[1].Split(":")[1].Trim().Split(" ").Where(x => !string.IsNullOrEmpty(x)).ToArray();
-        var index = 0;
-        for (int i = 0; i < times.Length; i++)
+        var wins = 0;
+        var winningTotals = new List<int>();
+
+        for (var i = 0; i < times.Length; i++)
         {
-            var raceTime = times[i];
-            var raceDistance = distances[i];
-            
-            
+            var raceTime = int.Parse(times[i]);
+            var raceDistance = int.Parse(distances[i]);
+
+            for (var speed = 0; speed < raceTime; speed++)
+            {
+                //v * T = d
+                var didIMakeIt = 0;
+                var timeLeftToMove = raceTime - speed;
+                // if distance travel divided by the distance per millisecond
+                //var totalDistanceTravelled = speed * timeLeftToMove;
+
+                for (int j = 1; j <= timeLeftToMove; j++)
+                {
+                    didIMakeIt += speed;
+                }
+
+                if (didIMakeIt > raceDistance)
+                    wins++;
+
+                //var willIMakeIt = speed - raceTime;
+                //if (distanceTravelled >= raceDistance)
+            }
+
+            winningTotals.Add(wins);
+            wins = 0;
         }
 
+        var marginOfError = 1;
+        foreach (var value in winningTotals)
+        {
+            marginOfError *= value;
+        }
 
-        return "0";
+// each time I return the count of each race and then at the top I can multiple all the total counts togeteher to get the margin of error 
+        return $"{marginOfError}";
     }
+
+    // public int GetSpeed(int timePast)
+    // {
+    //     
+    // }
 
     public ValueTask<string> Part2(string[] input)
     {
-        return ValueTask.FromResult("");
+        var winning = HoldTheButtonAgain(input);
+        return ValueTask.FromResult($"{winning}");
+    }
+
+    public string HoldTheButtonAgain(string[] input)
+    {
+        var times = input[0].Split(":")[1].Trim().Replace(" ", "");
+        var distances = input[1].Split(":")[1].Trim().Replace(" ", "");
+        long wins = 0;
+
+        var raceTime = long.Parse(times);
+        var raceDistance = long.Parse(distances);
+
+        for (long speed = 0; speed < raceTime; speed++)
+        {
+            if (speed == 71516)
+            {
+                //Do nothing
+            }
+            //v * T = d
+            long didIMakeIt = 0;
+            long timeLeftToMove = raceTime - speed;
+            // if distance travel divided by the distance per millisecond
+            //var totalDistanceTravelled = speed * timeLeftToMove;
+
+
+            didIMakeIt = timeLeftToMove * speed;
+            
+
+            if (didIMakeIt > raceDistance)
+                wins++;
+        }
+
+        return $"{wins}";
     }
 }
--- Aoc.Tests/Day06Tests.cs
+++ Aoc.Tests/Day06Tests.cs
@@ -11,7 +11,8 @@ public class Day06Tests
     //#region Part1
     [Fact]
     public async Task Day06_Solve_1_Test1()
-    {//destination range start -- source range start  -- range length
+    {
+        //destination range start -- source range start  -- range length
         // seed number    -           
         var data = """
                    Time:      7  15   30
@@ -24,5 +25,40 @@ public class Day06Tests
 
         result.Should().Be("288");
     }
+    
+    [Fact]
+    public async Task Day06_Solve_1_Test2()
+    {
+        //destination range start -- source range start  -- range length
+        // seed number    -           
+        var data = """
+                   Time:      30
+                   Distance:  200
+                   """;
 
+        var input = data.Split("\n");
+
+        var result = await _sut.Part1(input);
+
+        result.Should().Be("9");
+    }    
+    
+    [Fact]
+    public async Task Day06_Solve_2_Test1()
+    {
+        //destination range start -- source range start  -- range length
+        // seed number    -           
+        var data = """
+                   Time:      7  15   30
+                   Distance:  9  40  200
+                   """;
+
+        var input = data.Split("\n");
+
+        var result = await _sut.Part2(input);
+
+        result.Should().Be("71503");
+    }    
+    
+    
 }
